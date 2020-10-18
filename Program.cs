8fd41874c2f97a6db2c4e09af3bd2d1667cc15e6
Chloe Robertson <chlo0chlo0ee@gmail.com>
2020-10-18 23:07:02 +0100

    create grid class with base values and format output

--- Program.cs
+++ Program.cs
@@ -6,7 +6,45 @@ namespace NoughtsAndCrossesConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var grid = new GameGrid();
+            grid.GridInit();
+
+            Console.WriteLine((grid));
+
         }
     }
+
+    public enum GridSymbols
+    {
+        X=1,
+        O=2,
+
+    }
+
+
+    public class GameGrid
+    {
+        public string[,] Grid =new string[3,3];
+
+        public void GridInit()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    Grid[i, j] = "~";
+                }
+            }s
+        }
+        public override string ToString()
+        {
+            return $"({Grid[0, 0]}) | ({Grid[0, 1]}) | ({Grid[0,2]}) {Environment.NewLine}" +
+                   $"----|-----|-----{Environment.NewLine}"+
+                   $"({Grid[1, 0]}) | ({Grid[1, 1]}) | ({Grid[1,2]}) {Environment.NewLine}" +
+                   $"----|-----|-----{Environment.NewLine}"+
+                   $"({Grid[2, 0]}) | ({Grid[2, 1]}) | ({Grid[2,2]}) {Environment.NewLine}";}
+        }
+
+
+
 }
